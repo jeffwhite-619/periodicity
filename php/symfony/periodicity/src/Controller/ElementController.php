65dbd4d2e27dc37bcd47fa61a3059dc0265189e0
Jeff White <thejeffwhite@gmail.com>
2020-07-18 15:03:40 -0700

    getting all element nodes for index page

--- php/symfony/periodicity/src/Controller/ElementController.php
+++ php/symfony/periodicity/src/Controller/ElementController.php
@@ -3,9 +3,9 @@
 namespace App\Controller;
 
 use App\Service\Neo4jService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-// use Neo4j\Neo4jBundle\Collector\Twig;
 use GraphAware\Bolt\Result\Type\Node;
 
 class ElementController extends AbstractController
@@ -27,23 +27,15 @@ class ElementController extends AbstractController
     public function index()
     {
         $results  = $this->service->all()->records();
+
         $allNodes = [];
-        foreach($results as $result) {
+        foreach ($results as $result) {
             $allNodes[] = $result->values()[0];
         }
+
         return $this->render('element/index.html.twig', [
             'controller_name' => 'ElementController',
             'allNodes'        => $allNodes,
         ]);
-    }
-
-    /**
-     * @Route("/atoms", name="atoms")
-     */
-    public function atoms()
-    {
-        $results = $this->service->getElements();
-        var_dump($results);
-        die('TEST');
     }
 }
